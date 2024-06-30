36cb2b845a1e799c8ce036ab49d1ac5365615e9c
ThiagoHG1 <thiagohgegers34@gmail.com>
2024-06-30 20:40:56 -0300

    adição das pastas python e webpage; Adição de código html, css e js para a webpage

---
+++ src/webpage/scripts.js
@@ -0,0 +1,7 @@
+let producturl;
+let productname;
+
+function addproduct() {
+    productname = document.getElementById('product-name-input').querySelector('input').value;
+    producturl = document.getElementById('product-url-input').querySelector('input').value;
+}
